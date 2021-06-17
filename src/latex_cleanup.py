256e89af9cc34b01ba8f905b955bd32d606099e6
Maximilian Schambach <maximilian.schambach@kit.edu>
2021-06-17 10:53:19 +0200

    Add dryrun, recursive and rm option, swith to Path

--- src/latex_cleanup.py
+++ src/latex_cleanup.py
@@ -15,10 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-"""Script and CLI to delete all temporary LaTex files"""
-import os
 import argparse
+import os
+from pathlib import Path
 
 # Argument setup
 parser = argparse.ArgumentParser(
@@ -28,34 +27,41 @@ parser.add_argument('-f', '--folder', type=str,
   default="./",
   help='Folder to clean up from latex files. Default: Current')
 
+parser.add_argument('-d', '--dry', action='store_true',
+                    help='Dry run. Prints a list of files that would be cleaned up.')
+
 parser.add_argument('-e', '--extensions', action='store_true',
                     help='Print a list of extensions that are considered temporary files.')
+
+parser.add_argument('-r', '--recursive', action='store_true',
+                    help='Recursively search subfolders for temporary files.')
+
+parser.add_argument('--rm', action='store_true',
+                    help='Use /bin/rm instead of trashing to delete files.')
 
 args = parser.parse_args()
 
 # Extract arguments
 folder = args.folder
+dry = args.dry
 show_ext = args.extensions
+recursive = args.recursive
+rm = args.rm
 
 # Get current path
-folderpath = os.path.abspath(folder)
+folderpath = Path(folder)
 
 
 def is_tex_folder(folderpath):
     """Determine if a folder contains any tex files.
-
     Args:
         folderpath: System path of folder.
-
     Returns:
         True, if folder contains at least one tex file, else False.
     """
 
-    # Go through folder
-    for file in os.listdir(folderpath):
-
-        if file.lower().endswith(".tex"):
-            return True
+    if [f for f in folderpath.glob("*.tex")]:
+        return True
 
     return False
 
@@ -64,45 +70,59 @@ def get_extensions():
     # Extensions to cleanup
     extensions = [".aux", ".auxlock", ".bbl", ".bcf", ".blg", ".brf", ".dvi",
                   ".glo", ".idx", ".ilg", ".ind", ".log", ".lot", ".lof", ".ist",
-                  ".out", ".ps", ".xml", ".synctex.gz", ".thm", ".md5", ".dpth",
+                  ".out", ".ps", ".xml", ".gz", ".thm", ".md5", ".dpth",
                   ".toc", ".nls", ".nlo", ".nav", ".snm", ".upa", ".fls", ".fdb_latexmk",
-                  ".xabbr", ".xglos", ".xmathop", ".xparam", ".xsubsup", ".xsym", ".xincludes", ".gls"]
+                  ".Xabbr", ".Xglos", ".Xmathop", ".Xparam", ".Xsubsup", ".Xsym", ".Xincludes", ".gls"]
+
     # Remove possible duplicates
     extensions = list(set(extensions))
-    extensions.sort()
 
     return extensions
 
 
-def latex_cleanup(folderpath, extensions):
-    """Cleanup temporary LaTex files in a LaTex folder.
+def get_excludes():
+    # Folders to exlcude from cleanup
+    excludes = [".git/", ".idea/"]
+    return excludes
 
+
+def latex_cleanup(folderpath, extensions, excludes, rm):
+    """Cleanup temporary LaTex files in a LaTex folder.
     The folder must contain at least one .tex file to be a LaTex folder.
     Otherwise, no files are deleted.
-
     Args:
         folderpath: System path to the folder.
-
         extensions: A list of file extensions considered temporary files.
-
     Returns: List of deleted files. Returns None if no files were deleted.
-
     """
 
-    file_list = []
+    # Get all temporary files
+    if recursive:
+        pattern = "**/*"
+    else:
+        pattern = "*"
 
-    # Go through folder
-    for file in os.listdir(folderpath):
-        # Check if file ends with extension
-        for ext in extensions:
-            if file.lower().endswith(ext):
-                file_list.append(os.path.join(folderpath, file))
+
+    def exclude(file):
+        res = False
+        for ex in excludes:
+            if ex in str(file):
+                return True
+        return False
+
+    files = [f for f in folderpath.glob(pattern)
+             if f.suffix in extensions \
+             and not exclude(f)]
 
     # Delete files, if .tex file and temporary files were found
-    if len(file_list) != 0:
-        for file in file_list:
-            os.system("gio trash '" + file + "'")
-        return file_list
+    if files:
+        for file in files:
+            if not dry:
+                if rm:
+                    os.system("/bin/rm '" + str(file) + "'")
+                else:
+                    os.system("gio trash '" + str(file) + "'")
+        return files
 
     else:
         return None
@@ -111,20 +131,31 @@ def latex_cleanup(folderpath, extensions):
 if __name__ == "__main__":
 
     extensions = get_extensions()
+    excludes = get_excludes()
 
     if show_ext:
         print(extensions)
 
     else:
         print("Cleaning up temporary LaTex files...")
+        if recursive:
+            print("Recursive cleanup...")
+            print("Excluding folders", excludes)
+        if dry:
+            print("Dry run. Will not delete any files.")
 
-        if is_tex_folder(folder):
-            res = latex_cleanup(folder, extensions)
+
+        if is_tex_folder(folderpath):
+            res = latex_cleanup(folderpath, extensions, excludes, rm)
             if res is None:
                 print("No temporary LaTex files found.")
 
             else:
-                print("Trashed ", len(res), " temporary files.")
+                if dry:
+                    print("Found ", len(res), "temporary files:")
+                    print([str(f) for f in res])
+                else:
+                    print("Trashed ", len(res), " temporary files.")
 
         else:
             print("No files were cleaned up. The folder is not a LaTex folder.")
